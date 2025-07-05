fa28cfed05687c5e5ae8bb3eb3ac46beb1ebf639
Raheem <kamwangaraheem2050@gmail.com>
2025-07-05 19:59:24 +0300

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Change output directory
+    outDir: "dist", // Ensure output goes to dist directory
   },
 });
