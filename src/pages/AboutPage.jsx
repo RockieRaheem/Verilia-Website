ffb0ae3de1f12445af1e106da82c5f4847409409
Raheem <kamwangaraheem2050@gmail.com>
2025-07-07 14:33:32 +0300

    Create about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/AboutPage.jsx
+import React from "react";
+import About from "../components/About";
+
+const AboutPage = () => {
+  return (
+    <>
+      <About />
+    </>
+  );
+};
+
+export default AboutPage;
