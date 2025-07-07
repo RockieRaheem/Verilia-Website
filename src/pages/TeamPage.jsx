8eca50b6ae1116aed5fdf41b52b209d08af6c763
Raheem <kamwangaraheem2050@gmail.com>
2025-07-07 14:38:56 +0300

    Update team page

---
+++ src/pages/TeamPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/TeamPage.jsx
+import React from "react";
+import Team from "../components/Team";
+
+const TeamPage = () => {
+  return (
+    <>
+      <Team />
+    </>
+  );
+};
+
+export default TeamPage;
